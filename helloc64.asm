8385c6fa093ee182ad9abb4b84992d7853a5ad51
Stefano Caronia <stefano.caronia@gmail.com>
2026-01-11 15:15:07 +0100

    Exercise: Clear screen, loop with characters and colors

--- helloc64.asm
+++ helloc64.asm
@@ -6,6 +6,14 @@ BasicUpstart2(start)
 .const BACKGROUND_COLOR = $D021
 
 start:
+    // Clear screen
+    lda #$93      // CLR/HOME (clear screen)
+    jsr $ffd2     // KERNAL CHROUT
+    
+    // Switch to lowercase/uppercase charset
+    // lda #$0E
+    // jsr $ffd2
+
     // 1) Imposta colori bordo/sfondo (VIC-II)
     lda #$0                 // 0 = nero
     sta BORDER_COLOR        // border color
@@ -15,7 +23,7 @@ start:
     // 2) Scrivi un carattere a schermo
     lda #$53
     sta SCREEN + 30
-    lda #$2
+    lda #$7
     sta COLOR + 30
 
 // 3) Scrivi 16 caratteri ognuno con un colore diverso

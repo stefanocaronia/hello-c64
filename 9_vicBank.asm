387a52c97d5d14fa027ff9554487d7bb2bca47de
Stefano Caronia <stefano.caronia@gmail.com>
2026-01-25 22:58:03 +0100

    Imported a custom charset

--- 9_vicBank.asm
+++ 9_vicBank.asm
@@ -4,7 +4,8 @@ BasicUpstart2(start)
 
 // TODO: definisci le costanti che ti servono
 .const SCREEN = $4400
-.const CHAR = $53 // cuoricino
+.const CHAR = $01
+.const CHARSET = $4000 
 
 start:
     // 1. Seleziona bank 1 via $DD00
@@ -18,12 +19,45 @@ start:
 
     lda $d018
     and #%00001111
-    ora #%00010000
+    ora #%00010001
     sta $d018
 
+    // Imposta charset a offset $4000 (mantenendo schermo)
+    lda $D018
+    and #%11110001
+    ora #%00000000    // %100 << 1 = %1000
+    sta $D018
+
+    jsr ClearScreen
 
     // 3. Scrivi un carattere a $4400
     lda #CHAR
     sta SCREEN
 
+    lda #CHAR + 1 
+    sta SCREEN +1 
+
     jmp *
+
+    ClearScreen: {
+        ldx #$00
+    loop: 
+        lda #$20
+        sta SCREEN,x
+        sta SCREEN+$0100,x // 256
+        sta SCREEN+$0200,x // 512
+        sta SCREEN+$0300,x // 768
+        sta SCREEN,x
+        sta SCREEN+$0100,x // 256
+        sta SCREEN+$0200,x // 512
+        sta SCREEN+$0300,x // 768
+        inx
+        bne loop
+        rts
+    }
+
+*=CHARSET "Charset"
+.import binary "charsets/charset-1.bin"
+
+
+
---
+++ Doc/onlinetools.md
@@ -0,0 +1,1 @@
+https://kickassmemoryview.goatpower.org/
